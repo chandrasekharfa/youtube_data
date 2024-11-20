d015256546ac3f1559f02f914511e4fb2914c593
CHANDRASEKHAR KARRI <141818864+chandrasekharfa@users.noreply.github.com>
2024-11-20 15:28:53 +0530

    Update database.py

--- database.py
+++ database.py
@@ -8,9 +8,7 @@ db = client["youtube_stats"]
 collection = db["video_stats"]
 
 def save_video_stats_to_db(video_data):
-    """
-    Save new video stats to the MongoDB collection.
-    """
+    
     try:
         video_data["created_at"] = datetime.utcnow()
         video_data["last_updated_at"] = datetime.utcnow()
@@ -21,9 +19,7 @@ def save_video_stats_to_db(video_data):
 
 
 def update_video_stats_in_db(video_id, updated_data):
-    """
-    Update existing video stats in the MongoDB collection.
-    """
+    
     collection.update_one(
         {"_id": video_id},
         {"$set": updated_data}
